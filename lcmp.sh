5ea8a2f88970236d42c81be65fc73b9279e066c4
MoeWhale <51768867+moewhale@users.noreply.github.com>
2026-01-06 13:55:57 +0800

    Update lcmp.sh

--- lcmp.sh
+++ lcmp.sh
@@ -496,8 +496,8 @@ _info "Caddy configuration completed"
 _info "Downloading and running MariaDB repository setup script"
 _error_detect "curl -sLo mariadb_repo_setup.sh https://dl.lamp.sh/files/mariadb_repo_setup.sh"
 
-###
-sed 's/\/repo\/mariadb-server/\/browse\/mariadb_server/g' mariadb_repo_setup.sh
+# 使用 | 作为分隔符，代码更清晰
+sed -i 's|/repo/mariadb-server|/browse/mariadb_server|g' mariadb_repo_setup.sh
 
 _error_detect "chmod +x mariadb_repo_setup.sh"
 # Fix MariaDB package signing key import error
